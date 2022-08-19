ce7a4ed5b279cb454fcd5445d75c6004c8d49bcc
Stanislav <stanislav.samarin@gmail.com>
2022-08-19 16:02:50 +0300

    Написал программу

--- Ex4/Program.cs
+++ Ex4/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число a:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c:");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max) {
+    max = b;
+}
+if (c > max) {
+    max = c;
+}
+Console.Write("Максимальое число - ");
+Console.WriteLine(max);
