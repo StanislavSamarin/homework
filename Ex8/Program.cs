1880701a2a34a17d2a6b09c5c64bb8316e353e18
Stanislav <stanislav.samarin@gmail.com>
2022-08-19 16:52:15 +0300

    Написал программу

--- Ex8/Program.cs
+++ Ex8/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i <= N) {
+    if (i % 2 == 0) {
+        Console.WriteLine(i);
+    }
+    i++;
+}
