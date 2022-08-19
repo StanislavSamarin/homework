d5856c4169086d69c2abbf04ecf553e6883aff19
Stanislav <stanislav.samarin@gmail.com>
2022-08-19 17:57:55 +0300

    Доработал программу

--- Ex8/Program.cs
+++ Ex8/Program.cs
@@ -1,9 +1,16 @@
-﻿Console.WriteLine("Введите число:");
+﻿// Console.WriteLine("Введите число:");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int i = 1;
+// while (i <= N) {
+//     if (i % 2 == 0) {
+//         Console.WriteLine(i);
+//     }
+//     i++;
+// }
+Console.WriteLine("Введите число:");
 int N = Convert.ToInt32(Console.ReadLine());
-int i = 1;
+int i = 2;
 while (i <= N) {
-    if (i % 2 == 0) {
-        Console.WriteLine(i);
-    }
-    i++;
+    Console.WriteLine(i);
+    i = i + 2;
 }
