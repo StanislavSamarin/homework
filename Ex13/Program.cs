7bfc11810af6fe9672d1d2f4f3dff8bb65a9ecdc
Stanislav <stanislav.samarin@gmail.com>
2022-08-25 02:11:38 +0300

    Написал программу

--- Ex13/Program.cs
+++ Ex13/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿int thirdNumber(int number)
+{
+    int numberNext = number;
+    int number3 = numberNext;
+    if (number / 100 > 0)
+    {
+        while (numberNext / 100 > 0)
+        {
+            number3 = numberNext % 10;
+            numberNext = numberNext / 10;
+        }
+        return number3;
+    }
+    else
+    {
+        int error = -1;
+        return error;
+    }
+}
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = thirdNumber(number);
+
+if (result == -1)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine(result);
+}
