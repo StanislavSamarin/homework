a303a9366e0dc7bd49151688c5d40e8ae157d2aa
Stanislav <stanislav.samarin@gmail.com>
2022-08-24 21:37:47 +0300

    Написал программу

--- Ex10/Program.cs
+++ Ex10/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿int middleNumber(int number)
+{
+    if (number / 1000 == 0 && number / 100 > 0)
+    {
+        int middleNumber = number % 100 / 10;
+        return middleNumber;
+    }
+    else
+    {
+        int error = -1;
+        return error;
+    }
+}
+
+Console.WriteLine("Введите трёхзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = middleNumber(number);
+
+if (result == -1)
+{
+    Console.WriteLine("Ошибка: Вы ввели не трёхзначное число. Снова запустите программу и введите трёхзначное число.");
+}
+else
+{
+    Console.WriteLine(result);
+}
