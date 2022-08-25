5ca2995be97f492019609d0225c87dea5d97674b
Stanislav <stanislav.samarin@gmail.com>
2022-08-25 17:26:41 +0300

    Доработал программу

--- Ex15/Program.cs
+++ Ex15/Program.cs
@@ -1,27 +1,29 @@
-﻿string weekends(int number)
+﻿bool CheckWeekends(int number)
 {
-    string yes = "Да";
-    string no = "Нет";
     if (number == 6 || number == 7)
     {
-        return yes;
+        return true;
     }
     else
     {
-        return no;
+        return false;
     }
 }
 
 Console.WriteLine("Введите цифру от 1 до 7:");
 int number = Convert.ToInt32(Console.ReadLine());
 
-string result = weekends(number);
+bool result = CheckWeekends(number);
 
-if (number > 0 && number < 8)
-{
-    Console.WriteLine(result);
-}
-else
+if (number < 1 || number > 7)
 {
     Console.WriteLine("Ошибка! Снова запустите программу и введите цифру от 1 до 7:");
+}
+else if (result == true)
+{
+    Console.WriteLine("Да");
+}
+else if (result == false)
+{
+    Console.WriteLine("Нет");
 }
