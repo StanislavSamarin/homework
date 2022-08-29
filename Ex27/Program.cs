ab205280579196dee7b6c112e7f8298a49b2615b
Stanislav <stanislav.samarin@gmail.com>
2022-08-30 01:09:51 +0300

    Wrote the program code

--- Ex27/Program.cs
+++ Ex27/Program.cs
@@ -3,3 +3,26 @@
 // 82 -> 10
 // 9012 -> 12
 
+Console.WriteLine("Введите натуральное число:");
+bool isNumberN = int.TryParse(Console.ReadLine(), out int N);
+
+if (isNumberN != true || N <= 0)
+{
+    Console.WriteLine("Ошибка! Вы ввели не натуральное число!");
+    return;
+}
+
+int GetSum(int N)
+{
+    int result = 0;
+    int number;
+    while (N > 0)
+    {
+        number = N % 10;
+        result = result + number;
+        N /= 10;
+    }
+    return result;
+}
+
+Console.WriteLine(GetSum(N));
