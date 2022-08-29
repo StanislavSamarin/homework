0d3de36a1dce1080e3c115158b87c74dc1a5f880
Stanislav <stanislav.samarin@gmail.com>
2022-08-29 17:18:19 +0300

    Wrote the program code

--- Ex23/Program.cs
+++ Ex23/Program.cs
@@ -2,3 +2,27 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
+Console.WriteLine("Введите положительное число: ");
+bool isNumberN = int.TryParse(Console.ReadLine(), out int N);
+
+if (isNumberN != true)
+{
+    Console.WriteLine("Ошибка! Вы ввели не число!");
+    return;
+}
+
+if (N < 0)
+{
+    Console.WriteLine("Ошибка! Вы ввели отрицательное число!");
+    return;
+}
+
+void GetCube(int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+    Console.WriteLine(Math.Pow((i + 1), 3));
+    }
+}
+
+GetCube(N);
