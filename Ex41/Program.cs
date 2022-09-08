2c463fe742b7d562e5beab8aa9485dfbb957daf0
Stanislav <stanislav.samarin@gmail.com>
2022-09-08 23:31:04 +0300

    Wrote the program code

--- Ex41/Program.cs
+++ Ex41/Program.cs
@@ -1,1 +1,55 @@
-﻿
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите количество чисел:");
+bool isNumberLength = int.TryParse(Console.ReadLine(), out int length);
+
+if (isNumberLength != true || length <= 0)
+{
+    Console.WriteLine("Ошибка! Вы ввели не число, либо оно отрицательное!");
+    return;
+}
+
+// Получили количество вводимых чисел
+
+int[] FillArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        Console.WriteLine($"Введите число {i + 1}: ");
+        bool isNumber = int.TryParse(Console.ReadLine(), out int number);
+        if (isNumber != true)
+        {
+            Console.WriteLine("Ошибка! Вы ввели не число!");
+            array[i] = -1;
+            return array;
+        }
+        array[i] = number;
+    }
+    return array;
+}
+
+int[] array = FillArray(length);
+
+// Получили массив введённых чисел
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+int count = CountPositiveNumbers(array);
+Console.WriteLine($"Количество положительных чисел: {count}");
+
+// Получили количество положительных чисел среди введённых
