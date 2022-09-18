aa0dc3ab0109ddafc8e5b061dd1021ec008cfb24
Stanislav <stanislav.samarin@gmail.com>
2022-09-18 16:34:51 +0300

    Wrote the program code

--- Ex54/Program.cs
+++ Ex54/Program.cs
@@ -7,3 +7,61 @@
 // 7 4 2 1
 // 9 5 3 2
 // 8 4 4 2
+
+int[,] array = CreateRandomArray(4, 4);
+
+Print2DArray(array);
+
+int[,] arrayNew = FillArrayNew(array);
+
+Console.WriteLine();
+
+Print2DArray(arrayNew);
+
+int[,] CreateRandomArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] FillArrayNew(int[,] array)
+{
+    int tmp;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
+        {
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, j] < array[i, k])
+                {
+                    tmp = array[i, j];
+                    array[i, j] = array[i, k];
+                    array[i, k] = tmp;
+                }
+            }
+        }
+    }
+    return array;
+}
