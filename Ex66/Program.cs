fb3868c7034fad8fb98573c903d593e97af09ce6
Stanislav <stanislav.samarin@gmail.com>
2022-09-22 13:39:12 +0300

    Wrote the program code

--- Ex66/Program.cs
+++ Ex66/Program.cs
@@ -4,3 +4,35 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+Console.WriteLine("Введите число m: ");
+bool isNumberM = int.TryParse(Console.ReadLine(), out int m);
+
+if (isNumberM != true)
+{
+    Console.WriteLine("Ошибка! Вы ввели не число!");
+    return;
+}
+
+Console.WriteLine("Введите число n: ");
+bool isNumberN = int.TryParse(Console.ReadLine(), out int n);
+
+if (isNumberM != true)
+{
+    Console.WriteLine("Ошибка! Вы ввели не число!");
+    return;
+}
+
+int GetSum(int m, int n)
+{
+    if (m > n)
+    {
+        return 0;
+    }
+    else
+    {
+        return m + GetSum(++m, n);
+    }
+}
+
+int sum = GetSum(m, n);
+Console.WriteLine(sum);
